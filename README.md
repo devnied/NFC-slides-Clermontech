ce50ad0413f47c0df842b1c8c73c1309063e5775
devnied <mxjulien@gmail.com>
2013-07-14 16:29:17 +0200

    Switch to gh-pages

---
+++ README.md
@@ -0,0 +1,12 @@
+NFC Slides
+======================
+
+NFC slides for for Clermont'ech API Hours #3
+
+Date: 3/7/2013
+
+# Display slides
+
+[Clermont'ech Slides](https://rawgithub.com/devnied/NFC-slides-Clermontech/master/template.html)
+
+
---
+++ slide_config.js
@@ -0,0 +1,32 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Mobilité et NFC dans les transports',
+    subtitle: '',
+    eventInfo: {
+      title: 'CLERMONT\'ECH API HOUR #3',
+      date: '3/7/2013'
+    },
+    useBuilds: true,
+    usePrettify: true,
+    enableSlideAreas: true,
+    enableTouch: true,
+    favIcon: 'images/favicon.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Julien MILLAU - @devnied',
+    company: '',
+    gplus: '',
+    twitter: '@devnied',
+    www: 'http://www.julien-millau.fr',
+    github: 'http://github.com/devnied'
+  }]
+};
+
